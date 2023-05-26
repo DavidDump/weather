000bfead94dc8eaaad1dbefed792e7c673a357f9
David Benes <dbenes10@gmail.com>
2023-05-26 03:01:19 +0200

    remapped path / to /index.html

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseStaticFiles();
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Redirect("/index.html"));
 
 app.Run();
